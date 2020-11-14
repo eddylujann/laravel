3f047a36e41308944b1435873a6539c85d9c225d
Eddy <eddylujann@gmail.com>
2020-11-14 02:56:21 +0100

    commit de local 2:56

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -100,7 +100,7 @@
                         </div>
                     </div>
                 </div>
-                <!--                        
+
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
@@ -125,7 +125,7 @@
                     <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
                     </div>
-                </div>-->
+                </div>
             </div>
         </div>
     </body>
